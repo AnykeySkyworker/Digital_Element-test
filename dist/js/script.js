aa9a23cdb7412e81cf12a0fb7b1015f519c77246
feelskill <maiersa@mail.ru>
2020-01-20 13:10:21 +0500

    небольшие правки к стилям и скрипту

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,26 +8,3 @@ let menuList = document.querySelector('.header__link-list');
         menuList.classList.toggle('header__link-list_hidden');
     });
 
-
-
-
-
-
-// document.addEventListener('click', function(event) {
-//     const id = event.target.dataset.toggleId;
-//     let menu = document.querySelector('.header__menu-img');
-//     if (!id) return;
-
-    // menu.addEventListener('click', () => {
-    //   menu.classList.toggle('header__menu_active');
-    //   menu.classList.toggle('header__menu_passive');
-    // });
-
-    // if (title.innerText == 'HIDE') {
-    //   title.innerText = 'SHOW'
-    // } else {
-    //   title.innerText = 'HIDE'
-    // }
-  //   let elem = document.getElementById(id);
-  //   elem.hidden = !elem.hidden;
-  // });
